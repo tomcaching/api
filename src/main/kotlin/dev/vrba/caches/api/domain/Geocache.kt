12e12deb23fb8236f31010d515056d9f680db7b7
Jiří Vrba <jiri@vrba.dev>
2023-01-14 22:04:20 +0100

    Add domain class for geocaches

---
+++ src/main/kotlin/dev/vrba/caches/api/domain/Geocache.kt
@@ -0,0 +1,45 @@
+package dev.vrba.caches.api.domain
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Column
+import org.springframework.data.relational.core.mapping.Table
+
+@Table("caches")
+data class Geocache(
+    @Id
+    @Column("id")
+    val id: Int,
+
+    @Column("cache_type")
+    val type: String,
+
+    @Column("cache_title")
+    val title: String,
+
+    @Column("cache_content")
+    val content: String,
+
+    @Column("cache_found")
+    val found: Boolean,
+
+    @Column("cache_locked")
+    val locked: Boolean,
+
+    @Column("coordinates_latitude")
+    val latitude: Double,
+
+    @Column("coordinates_longitude")
+    val longitude: Double,
+
+    @Column("cache_question")
+    val question: String? = null,
+
+    @Column("cache_solution")
+    val solution: String? = null,
+
+    @Column("fake_coordinates_latitude")
+    val fakeLatitude: Double? = null,
+
+    @Column("fake_coordinates_longitude")
+    val fakeLongitude: Double? = null
+)
