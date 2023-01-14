cfc94622b7c617e1b8dc8f08c9054e049fa322c6
Jiří Vrba <jiri@vrba.dev>
2023-01-14 23:20:33 +0100

    Enable cross origin for the geocaches controller

--- src/main/kotlin/dev/vrba/caches/api/controller/GeocachesController.kt
+++ src/main/kotlin/dev/vrba/caches/api/controller/GeocachesController.kt
@@ -6,14 +6,11 @@ import dev.vrba.caches.api.request.CreateGeocacheRequest
 import dev.vrba.caches.api.service.GeocachesService
 import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/caches")
+@CrossOrigin(origins = ["*"])
 class GeocachesController(private val service: GeocachesService) {
 
     @GetMapping
