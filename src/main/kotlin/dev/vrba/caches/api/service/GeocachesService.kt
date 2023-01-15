86f669645c49e3e52195dd6ae82d1198e86571f5
Jiří Vrba <jiri@vrba.dev>
2023-01-15 22:31:50 +0100

    Validate geocache parameters

--- src/main/kotlin/dev/vrba/caches/api/service/GeocachesService.kt
+++ src/main/kotlin/dev/vrba/caches/api/service/GeocachesService.kt
@@ -1,6 +1,7 @@
 package dev.vrba.caches.api.service
 
 import dev.vrba.caches.api.domain.Geocache
+import dev.vrba.caches.api.exception.InvalidGeocacheParametersException
 import dev.vrba.caches.api.repository.GeocacheRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -24,6 +25,9 @@ class GeocachesService(private val repository: GeocacheRepository) {
         fakeLatitude: Double? = null,
         fakeLongitude: Double? = null
     ) {
+        validateGeocacheParameters(type, question, solution, fakeLatitude, fakeLongitude)
+
+        val locked = type === "mystery"
         val geocache = Geocache(
             id = 0,
             type = type,
@@ -35,11 +39,22 @@ class GeocachesService(private val repository: GeocacheRepository) {
             fakeLongitude = fakeLongitude,
             question = question,
             solution = solution,
-            locked = (solution != null),
+            locked = locked,
             found = false,
         )
 
         withContext(Dispatchers.IO) { repository.save(geocache) }
+    }
+
+    private fun validateGeocacheParameters(type: String, question: String?, solution: String?, fakeLatitude: Double?, fakeLongitude: Double?) {
+        if (type == "traditional") {
+            return
+        }
+
+        // Mystery caches must have all the following set
+        if (question == null || solution == null || fakeLatitude == null || fakeLongitude == null) {
+            throw InvalidGeocacheParametersException
+        }
     }
 
 }
