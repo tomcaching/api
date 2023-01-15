6a1f88fc0960b3c5caa68eec0927038283fd23e3
Jiří Vrba <jiri@vrba.dev>
2023-01-15 22:15:50 +0100

    Remove unused import

--- src/main/kotlin/dev/vrba/caches/api/controller/HomepageController.kt
+++ src/main/kotlin/dev/vrba/caches/api/controller/HomepageController.kt
@@ -5,7 +5,6 @@ import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
 import java.net.URI
 
 @Controller
